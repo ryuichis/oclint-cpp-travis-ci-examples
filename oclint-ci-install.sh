70ec93466c1f68b6182026335b5e04b90a9c986c
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-01-22 17:11:55 -0800

    Add installation script for CI

---
+++ oclint-ci-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Automatically installs OCLint.
+# This script was designed for usage in CI systems.
+
+curl -OL https://github.com/oclint/oclint/releases/download/v0.10.2/oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz 
+mkdir -p ~/oclint
+tar xf oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz -C ~/oclint --strip-components=1
+rm oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz
+export OCLINT_HOME="$HOME/oclint"
+export PATH="$OCLINT_HOME/bin:$PATH"
