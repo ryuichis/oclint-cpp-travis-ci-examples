9279af3b433070abd050d6ca5cfd3ea509bd9d7c
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-06-19 09:46:37 -0700

    Upgrade to v0.10.3

--- oclint-ci-install.sh
+++ oclint-ci-install.sh
@@ -2,9 +2,9 @@
 # Automatically installs OCLint.
 # This script was designed for usage in CI systems.
 
-curl -OL https://github.com/oclint/oclint/releases/download/v0.10.2/oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz 
+curl -OL https://github.com/oclint/oclint/releases/download/v0.10.3/oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
 mkdir -p ~/oclint
-tar xf oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz -C ~/oclint --strip-components=1
-rm oclint-0.10.2-x86_64-linux-3.13.0-48-generic.tar.gz
+tar xf oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz -C ~/oclint --strip-components=1
+rm oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
 export OCLINT_HOME="$HOME/oclint"
 export PATH="$OCLINT_HOME/bin:$PATH"
