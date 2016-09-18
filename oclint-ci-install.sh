fd098d15786e2d029ea1d7a79ae6336730b09c9e
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-09-18 09:53:57 -0500

    Replace with build on ubuntu 14.04

--- oclint-ci-install.sh
+++ oclint-ci-install.sh
@@ -2,9 +2,9 @@
 # Automatically installs OCLint.
 # This script was designed for usage in CI systems.
 
-curl -OL http://archives.oclint.org/releases/0.11/oclint-0.11-x86_64-linux-4.4.0-36-generic.tar.gz
+curl -OL http://archives.oclint.org/releases/0.11/oclint-0.11-x86_64-linux-3.13.0-92-generic.tar.gz
 mkdir -p ~/oclint
-tar xf oclint-0.11-x86_64-linux-4.4.0-36-generic.tar.gz -C ~/oclint --strip-components=1
-rm oclint-0.11-x86_64-linux-4.4.0-36-generic.tar.gz
+tar xf oclint-0.11-x86_64-linux-3.13.0-92-generic.tar.gz -C ~/oclint --strip-components=1
+rm oclint-0.11-x86_64-linux-3.13.0-92-generic.tar.gz
 export OCLINT_HOME="$HOME/oclint"
 export PATH="$OCLINT_HOME/bin:$PATH"
